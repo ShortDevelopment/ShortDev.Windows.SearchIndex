bc29b1174f9cef86ffb1f962986bece6f807302a
Lukas Kurz <lukas@shortdev.de>
2022-03-03 15:08:03 +0100

    Add project files.

---
+++ SearchCatalog.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Search.Interop;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ShortDev.Windows.SearchIndex
+{
+    public sealed class SearchCatalog
+    {
+        public string CatalogId { get; private set; }
+
+        public CSearchManager SearchManager { get; private set; }
+        public CSearchCatalogManager CatalogManager { get; private set; }
+        public CSearchQueryHelper SearchQueryHelper { get; private set; }
+
+        /// <summary>
+        /// See <see cref="SystemIndex"/> for default instance
+        /// </summary>
+        public SearchCatalog(string catalogId)
+        {
+            this.CatalogId = catalogId;
+            SearchManager = new();
+            CatalogManager = SearchManager.GetCatalog(catalogId);
+            SearchQueryHelper = CatalogManager.GetQueryHelper();
+        }
+
+        public static SearchCatalog SystemIndex { get => new("SystemIndex"); }
+
+        public DataTable Query(SearchQueryBuilder queryBuilder)
+            => Query(queryBuilder.Build(this.SearchQueryHelper));
+
+        public DataTable Query(string sqlQuery)
+        {
+            using (OleDbConnection Connection = new(SearchQueryHelper.ConnectionString))
+            {
+                Connection.Open();
+                OleDbCommand cmd = new(sqlQuery, Connection);
+                cmd.CommandType = CommandType.Text;
+                using (var reader = cmd.ExecuteReader())
+                {
+                    DataTable data = new();
+                    data.Load(reader);
+                    return data;
+                }
+            }
+        }
+    }
+}
---
+++ SearchQueryBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Search.Interop;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShortDev.Windows.SearchIndex
+{
+    public sealed class SearchQueryBuilder
+    {
+        public string UserQuery { get; private set; }
+        public SearchQueryBuilder(string userQuery)
+        {
+            this.UserQuery = userQuery;
+        }
+
+        public List<SearchIndexColumn> Columns { get; } = new();
+        public string WhereRestrictions { get; set; } = "";
+
+        public SearchQueryBuilder AddColumn(SearchIndexColumn column)
+        {
+            Columns.Add(column);
+            return this;
+        }
+
+        public SearchQueryBuilder UseFreeText(string keyword)
+        {
+            WhereRestrictions = $"or FREETEXT('{keyword}')";
+            return this;
+        }
+
+        public string Build(ISearchQueryHelper queryHelper)
+        {
+            queryHelper.QuerySelectColumns = string.Join(",", Columns.Select((x) => x.Name));
+            queryHelper.QueryWhereRestrictions = WhereRestrictions;
+            return queryHelper.GenerateSQLFromUserQuery(UserQuery);
+        }
+    }
+}
